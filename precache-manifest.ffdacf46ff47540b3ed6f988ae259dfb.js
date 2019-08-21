505cd4aad72005e7eec1cb4321695ea0e42fa164
gabrypol <gabrielepolenta@gmail.com>
2019-08-22 01:18:44 +0200

    Updates

---
+++ precache-manifest.ffdacf46ff47540b3ed6f988ae259dfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62992b1e08df2e6beb2cb939053cb3f0",
+    "url": "/swissInstagramPage/index.html"
+  },
+  {
+    "revision": "14bc7ac5d08f995b8210",
+    "url": "/swissInstagramPage/static/css/main.4357ba7c.chunk.css"
+  },
+  {
+    "revision": "c3358893c0b5894acb54",
+    "url": "/swissInstagramPage/static/js/2.4ff9afb0.chunk.js"
+  },
+  {
+    "revision": "14bc7ac5d08f995b8210",
+    "url": "/swissInstagramPage/static/js/main.8236a7c8.chunk.js"
+  },
+  {
+    "revision": "9af7fba3d26b10ad295a",
+    "url": "/swissInstagramPage/static/js/runtime~main.48f04078.js"
+  }
+]);
